c8d5e449a6a55afeadcef02646bccd243ea409f6
leeyoungsub <liive29252@gmail.com>
2020-01-17 00:26:33 +0900

    Update LogisticRegression.py

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -1,29 +1,33 @@
 import numpy as np
 import tensorflow as tf
-load_data=np.loadtxt('./data-01.csv',delimiter=',')
+load_data=np.loadtxt('./diabetes.csv',delimiter=',')
 x_data=load_data[:,0:-1]
 t_data=load_data[:,[-1]]
 W=tf.Variable(tf.random_normal([x_data.shape[1],t_data.shape[1]]))
-
-x=tf.placeholder(tf.float32,[None,3])
-t=tf.placeholder(tf.float32,[None,1])
-learning_rate=1e-5
+b=tf.Variable(tf.random_normal([1]))
+x=tf.placeholder(tf.float32,[None,x_data.shape[1]])
+t=tf.placeholder(tf.float32,[None,t_data.shape[1]])
+learning_rate=1e-4
 optimizer=tf.train.GradientDescentOptimizer(learning_rate)
 #세션생성
 sess=tf.Session()
 
 
 
-y=tf.matmul(x,W)
-loss=tf.reduce_mean(tf.square(y-t))
+z=tf.matmul(x,W)+b
+y=tf.sigmoid(z)
+loss=-tf.reduce_mean(t*tf.log(y)+(1-t)*tf.log(1-y))
 train=optimizer.minimize(loss)
-
+predicted=tf.cast(y>0.5,dtype=tf.float32)
+accuracy=tf.reduce_mean(tf.cast(tf.equal(predicted,t),dtype=tf.float32))
 #변수 노드값 초기화
 sess.run(tf.global_variables_initializer())
 
-for step in range(10001):
-    loss_val,y_val,_=sess.run([loss,y,train],feed_dict={x:x_data,t:t_data})
+for step in range(20001):
+    loss_val,_=sess.run([loss,train],feed_dict={x:x_data,t:t_data})
     if step%500==0:
         print('step=',step,', loss=',loss_val)
-print(sess.run(y,feed_dict={x:[[100,98,81]]}))
+y_val,predicted_val,accuracy_val=sess.run([y,predicted,accuracy],feed_dict={x:x_data,t:t_data})
+print(y_val.shape, predicted_val.shape)
+print('\n',accuracy_val)
 sess.close()
